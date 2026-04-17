480ca39e5c19a6932870323d47704194322256de
joao-vitorCC <joao.vitor2017cc@discente.ufg.br>
2026-04-17 13:14:12 -0300

    Cliente multithreading

--- client.py
+++ client.py
@@ -1,10 +1,19 @@
 from socket  import *
 from constCS import * #-
+import threading
 
-comando = input(f"DIGITE UM COMANDO\n")
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode(comando))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+def conectar(comando):
+	s = socket(AF_INET, SOCK_STREAM)
+	s.connect((HOST, PORT)) # connect to server (block until accepted)
+	s.send(str.encode(comando))  # send some data
+	data = s.recv(1024)     # receive the response
+	print (bytes.decode(data))            # print the result
+	s.close()               # close the connection
+	
+
+while True:		
+	comando = input()
+	thread = threading.Thread(target=conectar, args=(comando,))
+	thread.start()
+
+
